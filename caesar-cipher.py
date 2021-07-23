6021b257ccff9806c660f5355d6df1531b292499
carsten-rossen <crossen3@gmail.com>
2021-07-23 05:54:32 +0000

    edits

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -9,15 +9,23 @@
 # Import Libraries
 import string
 
-
 # Define functions
-def encrypt(msg, transpose_value):
+
+def key_prompt():
+    key = input("Key (integer): ")
+    try:
+        int(key)
+    except ValueError:
+        print("Please only input an integer.")
+        key = key_prompt()
+    return int(key)
+
+def encrypt(msg, key):
     new_msg = ""
-    transpose_value = int(transpose_value) % 100
     for i in range(len(msg)):
         if msg[i] not in string.whitespace:
-            location = string.printable.index(msg[i])
-            new_char = string.printable[location + transpose_value]
+            location = char_list.index(msg[i])
+            new_char = char_list[(location + key) % len(char_list)]
             new_msg = new_msg + new_char
         else:
             new_msg = new_msg + msg[i]
@@ -25,11 +33,19 @@ def encrypt(msg, transpose_value):
 
 
 # Main
+
+char_list = []
+for i in range(len(string.ascii_uppercase)):
+    char_list.append(string.ascii_uppercase[i])
+for i in range(len(string.digits)):
+    char_list.append(string.digits[i])
+for i in range(len(string.punctuation)):
+    char_list.append(string.punctuation[i])
+
 msg = input("Message to encrypt: ")
 msg = msg.upper()
-transpose_value = input("Transpose by (positive or negative integer): ")
-msg = encrypt(msg, transpose_value)
-
+key = key_prompt()
+msg = encrypt(msg, key)
 print("Your encrypted message is: " + msg)
 
 # End
