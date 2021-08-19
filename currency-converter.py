13f3923b91a4d5840b49cecdf93237b9e8ed0990
Carsten Rossen <crossen3@gmail.com>
2021-08-19 15:20:06 -0700

    project creation

---
+++ currency-converter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Script Name: Currency Converter
+# Author Name: Carsten Rossen
+# Date of Latest Revision: 8/19/21 
+# Purpose: This program will convert between currencies using the a GUI interface. 
+# I will achieve this with the Tkinter library.
