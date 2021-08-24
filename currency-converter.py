c801a430ccc27a375b9f325f0ac07b3f4daa4476
Carsten Rossen <crossen3@gmail.com>
2021-08-24 14:50:02 -0700

    First GUI created

--- currency-converter.py
+++ currency-converter.py
@@ -5,3 +5,16 @@
 # Date of Latest Revision: 8/19/21 
 # Purpose: This program will convert between currencies using the a GUI interface. 
 # I will achieve this with the Tkinter library. (Learning from this video: https://www.youtube.com/watch?v=YXPyB4XeYLA)
+
+# Import libraries
+from tkinter import *
+
+
+# Declare variables
+root = Tk()
+main_label = Label(root, text="Currency Converter")
+
+
+# Main
+main_label.pack()
+root.mainloop()
