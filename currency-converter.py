8048ddba594232f47b0f4e5fa5437ce0bca26a51
Carsten Rossen <crossen3@gmail.com>
2021-08-27 15:43:47 -0700

    grid

--- currency-converter.py
+++ currency-converter.py
@@ -4,7 +4,7 @@
 # Author Name: Carsten Rossen
 # Date of Latest Revision: 8/19/21 
 # Purpose: This program will convert between currencies using the a GUI interface. 
-# I will achieve this with the Tkinter library. (Learning from this video: https://www.youtube.com/watch?v=YXPyB4XeYLA) 10:33
+# I will achieve this with the Tkinter library. (Learning from this video: https://www.youtube.com/watch?v=YXPyB4XeYLA) 19:30
 
 # Import libraries
 from tkinter import *
@@ -13,8 +13,10 @@ from tkinter import *
 # Declare variables
 root = Tk()
 main_label = Label(root, text="Currency Converter")
+description = Label(root, text="This program will converter currencies to other currencies.")
 
 
 # Main
-main_label.pack()
+main_label.grid(row=0, column=0)
+description.grid(row=1, column=0)
 root.mainloop()
